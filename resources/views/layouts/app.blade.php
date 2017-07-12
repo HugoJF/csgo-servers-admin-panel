4aa81be214bf54065c10cc00d1e1382b563daf5d
Hugo Jeller <hugo_jeller@hotmail.com>
2017-07-11 23:19:17 -0400

    Testing RunCloud autodeploy v2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ route('home') }}">CS:GO Admin Panel</a>
+            <a class="navbar-brand" href="{{ route('home') }}">CS:GO Admin Panel Alpha</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
