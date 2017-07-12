fc1e580ba144b715221a7fcad72f783ae50db32f
Hugo Jeller <hugo_jeller@hotmail.com>
2017-07-12 10:42:47 -0400

    Added table for daemon logs

---
+++ database/migrations/2017_07_12_142935_create_daemon_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDaemonLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daemon_logs', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('log');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daemon_logs');
+    }
+}
