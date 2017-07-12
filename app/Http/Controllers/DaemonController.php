9f16c532bcd4854bd6633f1cd9e67ad64cfb3858
Hugo Jeller <hugo_jeller@hotmail.com>
2017-07-12 11:20:45 -0400

    Added missing files

---
+++ app/Http/Controllers/DaemonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DaemonLog;
+use Illuminate\Http\Request;
+
+class DaemonController extends Controller
+{
+    public function logs() {
+        /*return view('daemon-logs', [
+            logs => DaemonLog::all()
+        ]);*/
+
+        return DaemonLog::all();
+    }
+}
